1558e504677f8a22bbd8540ce5d093c3aebc63ec
johnbr001 <san2738@gmail.com>
2023-04-16 12:35:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Button} from 'react-bootstrap'
+import {Form, Stack} from 'react-bootstrap'
 
 function App() {
+  const [result, setResult] = useState(""); // result of calculation
+  // new number to do calculation with existing result
+  const [num, setNum] = useState(""); 
+
+  const handleAdd = (e) => {
+    e.preventDefault(); // prevent going to certain link
+    setResult(Number(result) + Number(num));
+    setNum("");
+  };
+
+  const handleMinus = (e) => {
+    e.preventDefault();
+    setResult(Number(result) - Number(num));
+    setNum("");
+  };
+
+  const handleMultiply = (e) => {
+    e.preventDefault();
+    setResult(Number(result) * Number(num));
+    setNum("");
+  };
+
+  const handleDivide = (e) => {
+    e.preventDefault();
+    setResult(Number(result) / Number(num));
+    setNum("");
+  };
+
+  const handleResidual = (e) => {
+    e.preventDefault();
+    setResult(Number(result) % Number(num));
+    setNum("");
+  };
+
+  const handleClear = (e) => {
+    e.preventDefault();
+    setResult("");
+    setNum("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-title">
+        <h1> Calculator </h1>
+      </div>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Result</Form.Label>
+          <Form.Control value={result} readOnly></Form.Control>
+          <Form.Text className="text-muted" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Input Number</Form.Label>
+          <Form.Control value={num} 
+            placeholder="enter a number" onChange={(e) => setNum(e.target.value)} ></Form.Control>
+          <Form.Text className="text-muted" />  
+        </Form.Group>
+        
+        <Stack className="justify-content-center" direction="horizontal" gap={2}>
+          <Button class="btn btn-outline-primary mr-1" onClick={handleAdd}>Add</Button>
+          <Button class="btn btn-outline-primary" onClick={handleMinus}>Minus</Button>
+          <Button class="btn btn-outline-primary" onClick={handleMultiply}>Multiply</Button>
+          <Button class="btn btn-outline-primary" onClick={handleDivide}>Divide</Button>
+          <Button class="btn btn-outline-primary" onClick={handleResidual}>Residual</Button>
+          <Button class="btn btn-outline-primary" onClick={handleClear}>Clear</Button>
+        </Stack>
+      </Form>
     </div>
   );
 }
